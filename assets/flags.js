3c4d83b1b62a0ae280a66575ff46e68aa09402c6
Raíssa Bonfim <raissa230306@edu.unipar.br>
2026-05-05 20:01:26 -0300

    RF-001: cria arquivo separado para mapeamento das flags

---
+++ assets/flags.js
@@ -0,0 +1,9 @@
+const  flags = {
+        MEX: require('../assets/jogos/mexico.png'),
+        RSA: require('../assets/jogos/south africa.png'),
+        KOR: require('../assets/jogos/south korea.png'),
+        CZE: require('../assets/jogos/czech republic.png'),
+    }
+    export default flags;
+    //GameCard → só mostra a interface 
+//flags.js → guarda as imagens 
